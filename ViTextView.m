f0cf3c251e9ed7735707e36f745a0ef63e92e49d
Martin Hedenfalk <martin@bzero.se>
2008-10-29 07:25:42 +0100

    place cursor at first tabstop in snippet

--- ViTextView.m
+++ ViTextView.m
@@ -900,8 +900,6 @@ int logIndent = 0;
 	[shellVariableSet formUnionWithCharacterSet:[NSCharacterSet characterSetWithRange:NSMakeRange('A', 'Z' - 'A')]];
 	[shellVariableSet formUnionWithCharacterSet:[NSCharacterSet characterSetWithCharactersInString:@"_"]];
 
-	INFO(@"parsing placeholder [%@]", s);
-
 	BOOL bracedExpression = NO;
 	if ([scan scanString:@"{" intoString:nil])
 		bracedExpression = YES;
@@ -951,11 +949,12 @@ int logIndent = 0;
 {
         NSMutableString *s = [[NSMutableString alloc] initWithString:snippet];
 
+	NSMutableDictionary *tabstops = [[NSMutableDictionary alloc] init];
+
 	BOOL foundMarker;
 	NSUInteger i;
 	do
         {
-                INFO(@"parsing markers, s = [%@]", s);
                 foundMarker = NO;
                 for (i = 0; i < [s length]; i++)
                 {
@@ -966,13 +965,36 @@ int logIndent = 0;
                                 NSMutableDictionary *meta = [[NSMutableDictionary alloc] init];
                                 if (![self parseSnippetPlaceholder:[s substringFromIndex:i + 1] length:&len meta:meta])
                                 	break;
-
-                                // fixme: add (relative) location to meta
                                 
                                 if ([meta objectForKey:@"defaultValue"])
                                         [s replaceCharactersInRange:NSMakeRange(i, len + 1) withString:[meta objectForKey:@"defaultValue"]];
                                 else
                                         [s deleteCharactersInRange:NSMakeRange(i, len + 1)];
+
+				NSNumber *tabStop = [meta objectForKey:@"tabStop"];
+				if (tabStop)
+                        	{
+                                       NSMutableDictionary *d = [tabstops objectForKey:tabStop];
+                                       if (d)
+                                       {
+                                                /* merge tabstops, especially default value */
+                                                if ([meta objectForKey:@"defaultValue"])
+                                                        [d setObject:[meta objectForKey:@"defaultValue"] forKey:@"defaultValue"];
+                                                if ([meta objectForKey:@"transformation"])
+                                                        [d setObject:[meta objectForKey:@"transformation"] forKey:@"transformation"];
+                                                // FIXME: sort locations
+                                                [[d objectForKey:@"locations"] addObject:[NSNumber numberWithInteger:i]];
+                                        }
+                                        else
+                                        {
+                                                [tabstops setObject:meta forKey:tabStop];
+                                                NSMutableArray *locations = [[NSMutableArray alloc] init];
+                                                [locations addObject:[NSNumber numberWithInteger:i]];
+                                                [meta setObject:locations forKey:@"locations"];
+                                        }
+
+                                        INFO(@"parsed tabstop: %@", [tabstops objectForKey:tabStop]);
+                        	}
 
                                 foundMarker = YES;
                                 break;
@@ -981,18 +1003,28 @@ int logIndent = 0;
         } while (foundMarker);
 
         // FIXME: inefficient(?)
+        NSUInteger loc = aLocation;
         for (i = 0; i < [s length]; i++)
         {
                 if ([s characterAtIndex:i] == '\n')
                 {
-                        aLocation += [self insertNewlineAtLocation:aLocation indentForward:YES];
+                        loc += [self insertNewlineAtLocation:loc indentForward:YES];
                 }
                 else
                 {
-                        [self insertString:[s substringWithRange:NSMakeRange(i, 1)] atLocation:aLocation];
+                        [self insertString:[s substringWithRange:NSMakeRange(i, 1)] atLocation:loc];
                         insert_end_location++;
-                        aLocation++;
+                        loc++;
                 }
+        }
+
+        NSDictionary *firstTabStop = [tabstops objectForKey:[NSNumber numberWithInt:1]];
+        if (firstTabStop)
+        {
+                INFO(@"placing cursor at first tabstop %@", firstTabStop);
+                loc = [[[firstTabStop objectForKey:@"locations"] objectAtIndex:0] integerValue] + aLocation;
+                NSUInteger len = [[firstTabStop objectForKey:@"defaultValue"] length];
+                [self setSelectedRange:NSMakeRange(loc, len)];
         }
 }
 

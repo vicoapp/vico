e23172ccd18a63357accfffa5c8d3397662a544d
Martin Hedenfalk <martin@bzero.se>
2010-04-13 21:00:25 +0200

    release build scripts

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+tag=$1
+if test -z "$tag"; then
+	version=$(date +"%Y%m%d.%H%M")
+	tag=HEAD
+else
+	version=$(echo $tag | sed -e 's/^V//' -e 's/_/./g' )
+fi
+
+echo tag is $tag
+echo version = $version
+
+dir=build.xi-$version
+echo build directory is $dir
+if test -d "$dir"; then
+	echo "build dir already exists: $dir"
+	exit 1
+fi
+
+echo checking out sources
+cvs co -r $tag -d $dir xi || exit 2
+
+cd $dir
+./mkdmg $version || exit 3
+mv $dir/xi-$version.dmg .
+ls -lh xi-$version.dmg 
+

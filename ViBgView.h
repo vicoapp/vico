1a7c1d595081db658be62e94779a123707d9e3d2
Martin Hedenfalk <martin@bzero.se>
2010-05-02 10:28:27 +0200

    differentiate between regular background and source highlight background

--- ViBgView.h
+++ ViBgView.h
@@ -2,5 +2,7 @@
 
 @interface ViBgView : NSView
 {
+	BOOL sourceHighlight;
 }
+@property(readwrite) BOOL sourceHighlight;
 @end
--- ViBgView.m
+++ ViBgView.m
@@ -2,11 +2,15 @@
 
 @implementation ViBgView
 
+@synthesize sourceHighlight;
+
 - (void)drawRect:(NSRect)rect
 {
 	rect = [self bounds];
-	//[[NSColor colorWithDeviceRed:(float)0xDD/0xFF green:(float)0xE4/0xFF blue:(float)0xEB/0xFF alpha:1.0] set];
-	[[NSColor windowBackgroundColor] set];
+	if (sourceHighlight)
+		[[NSColor colorWithDeviceRed:(float)0xDD/0xFF green:(float)0xE4/0xFF blue:(float)0xEB/0xFF alpha:1.0] set];
+	else
+		[[NSColor colorWithDeviceRed:(float)0xED/0xFF green:(float)0xED/0xFF blue:(float)0xED/0xFF alpha:1.0] set];
 	[NSBezierPath fillRect: rect];
 }
 

4f9a5f077c426485a8f0fc3ea1b75aea2cfb8b37
Martin Hedenfalk <martin@bzero.se>
2010-09-06 16:16:22 +0200

    remove unused code

--- ViProject.m
+++ ViProject.m
@@ -3,12 +3,6 @@
 
 @implementation ViProject
 
-- (id)init
-{
-	self = [super init];
-	return self;
-}
-
 - (NSString *)windowNibName
 {
 	return @"ViDocument";
@@ -17,13 +11,7 @@
 - (void)makeWindowControllers
 {
 	windowController = [[ViWindowController alloc] init];
-	INFO(@"created window controller %@", windowController);
 	[self addWindowController:windowController];
-}
-
-- (void)windowControllerDidLoadNib:(NSWindowController *)aController
-{
-	INFO(@"window controller %@ loaded nib", aController);
 }
 
 @end

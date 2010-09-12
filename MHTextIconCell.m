826d70c99ef7c195b9d8629c60d190fb59fb6f25
Martin Hedenfalk <martin@bzero.se>
2010-09-12 09:08:59 +0200

    more space around icons

--- MHTextIconCell.m
+++ MHTextIconCell.m
@@ -11,11 +11,11 @@
 	imgRect.origin = cellFrame.origin;
 	imgRect.origin.x += 1; // two pixels space around width
 	imgRect.size = size;
-	if (imgRect.size.height < cellFrame.size.height)
-		imgRect.size.height += (cellFrame.size.height - size.height) / 2;
+//	if (imgRect.size.height < cellFrame.size.height)
+//		imgRect.size.height += (cellFrame.size.height - size.height) / 2;
 	[image setFlipped:YES];
 	[image drawInRect:imgRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
-	NSRect textRect = NSMakeRect(cellFrame.origin.x + size.width+4, cellFrame.origin.y, cellFrame.size.width - size.width+4, cellFrame.size.height);
+	NSRect textRect = NSMakeRect(cellFrame.origin.x + size.width+4, cellFrame.origin.y + 2, cellFrame.size.width - size.width+4, cellFrame.size.height);
 	[super drawInteriorWithFrame:textRect inView:controlView];
 }
 

ba66205d9b7f83859e207951345c85d452b414ec
Martin Hedenfalk <martin@bzero.se>
2010-09-14 09:41:19 +0200

    Only deny attaching debuggers in release builds.

--- main.m
+++ main.m
@@ -10,6 +10,8 @@ main(int argc, char *argv[])
 {
 	LogLevel ll = SYSLOG_LEVEL_DEBUG1 + 2;
 	log_init(argv[0], ll, SYSLOG_FACILITY_USER, 1);
+#if defined(RELEASE_BUILD)
 	ptrace(PT_DENY_ATTACH, 0, 0, 0);
+#endif
 	return NSApplicationMain(argc, (const char **) argv);
 }

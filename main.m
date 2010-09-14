f0bb0360a61b28c1a65abe9f87395ecf05dca177
Martin Hedenfalk <martin@bzero.se>
2010-09-14 09:22:00 +0200

    deny attaching a debugger to this process

--- main.m
+++ main.m
@@ -1,5 +1,8 @@
 #import <Cocoa/Cocoa.h>
 
+#include <sys/types.h>
+#include <sys/ptrace.h>
+
 #include "log.h"
 
 int
@@ -7,5 +10,6 @@ main(int argc, char *argv[])
 {
 	LogLevel ll = SYSLOG_LEVEL_DEBUG1 + 2;
 	log_init(argv[0], ll, SYSLOG_FACILITY_USER, 1);
+	ptrace(PT_DENY_ATTACH, 0, 0, 0);
 	return NSApplicationMain(argc, (const char **) argv);
 }

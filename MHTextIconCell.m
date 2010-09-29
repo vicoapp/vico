daa4bdaf62f47bcc0f43cb3d3649bdcfa76b233b
Martin Hedenfalk <martin@bzero.se>
2010-09-29 10:25:45 +0200

    more space horizontally

--- MHTextIconCell.m
+++ MHTextIconCell.m
@@ -9,14 +9,14 @@
 	NSSize size = [image size];
 	NSRect imgRect;
 	imgRect.origin = cellFrame.origin;
-	imgRect.origin.x += 1; // two pixels space around width
-	imgRect.origin.y += 2; // four pixels space around height
+	imgRect.origin.x += 4;
+	imgRect.origin.y += 2;
 	imgRect.size = size;
 //	if (imgRect.size.height < cellFrame.size.height)
 //		imgRect.size.height += (cellFrame.size.height - size.height) / 2;
 	[image setFlipped:YES];
 	[image drawInRect:imgRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:1.0];
-	NSRect textRect = NSMakeRect(cellFrame.origin.x + size.width+4, cellFrame.origin.y + 3.5, cellFrame.size.width - size.width+4, cellFrame.size.height);
+	NSRect textRect = NSMakeRect(cellFrame.origin.x + size.width+6, cellFrame.origin.y + 3.5, cellFrame.size.width - size.width+6, cellFrame.size.height);
 	[super drawInteriorWithFrame:textRect inView:controlView];
 }
 

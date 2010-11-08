d55c0fd77da8db455666b4d26cfe6a8aeca4ced0
Martin Hedenfalk <martin@bzero.se>
2010-11-08 21:54:09 +0100

    Only execute bash if bundle command doesn't start with a shebang. Set the caret position after inserting a snippet correctly.

--- ViTextView-bundle_commands.m
+++ ViTextView-bundle_commands.m
@@ -101,6 +101,7 @@
 
 - (void)setenv:(const char *)var value:(NSString *)value
 {
+	DEBUG(@"%s = [%@]", var, value);
 	if (value)
 		setenv(var, [value UTF8String], 1);
 }
@@ -135,7 +136,7 @@
 	[self setenv:"TM_FILENAME" value:[[[[self delegate] fileURL] path] lastPathComponent]];
 	[self setenv:"TM_FILEPATH" value:[[[self delegate] fileURL] path]];
 	[self setenv:"TM_FULLNAME" value:NSFullUserName()];
-	[self setenv:"TM_LINE_INDEX" integer:[self currentColumn]];
+	[self setenv:"TM_LINE_INDEX" integer:[[self textStorage] lineIndexAtLocation:[self caret]]];
 	[self setenv:"TM_LINE_NUMBER" integer:[self currentLine]];
 	[self setenv:"TM_SCOPE" value:[[self scopesAtLocation:[self caret]] componentsJoinedByString:@" "]];
 
@@ -155,8 +156,9 @@
 
 - (void)performBundleCommand:(id)sender
 {
-	NSDictionary *command = [sender representedObject];
-	INFO(@"command = %@", command);
+	NSDictionary *command = sender;
+	if ([sender respondsToSelector:@selector(representedObject)])
+		command = [sender representedObject];
 	NSRange inputRange;
 
 	/* FIXME: refactor ESC handling, and call it from here, ie both in insert and normal/visual mode, goto normal mode.
@@ -182,7 +184,7 @@
 	int fd = -1;
 
 	NSString *shellCommand = [command objectForKey:@"command"];
-//	if ([shellCommand hasPrefix:@"#!"])
+	if ([shellCommand hasPrefix:@"#!"])
 	{
 		const char *tmpl = [[NSTemporaryDirectory() stringByAppendingPathComponent:@"vibrant_cmd.XXXXXX"] fileSystemRepresentation];
 		DEBUG(@"using template %s", tmpl);
@@ -206,11 +208,15 @@
 		shellCommand = [[NSFileManager defaultManager] stringWithFileSystemRepresentation:templateFilename length:strlen(templateFilename)];
 	}
 
-	INFO(@"input text = [%@]", inputText);
+	DEBUG(@"input text = [%@]", inputText);
 
 	NSTask *task = [[NSTask alloc] init];
-	[task setLaunchPath:@"/bin/bash"];
-	[task setArguments:[NSArray arrayWithObjects:@"-c", shellCommand, nil]];
+	if (templateFilename)
+		[task setLaunchPath:shellCommand];
+	else {
+		[task setLaunchPath:@"/bin/bash"];
+		[task setArguments:[NSArray arrayWithObjects:@"-c", shellCommand, nil]];
+	}
 
 	id shellInput;
 	if ([inputText length] > 0)
@@ -263,7 +269,7 @@
 		NSData *outputData = [[shellOutput fileHandleForReading] readDataToEndOfFile];
 		NSString *outputText = [[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding];
 
-		INFO(@"command output: %@", outputText);
+		DEBUG(@"command output: %@", outputText);
 
 		if ([outputFormat isEqualToString:@"replaceSelectedText"])
 			[self replaceRange:inputRange withString:outputText undoGroup:NO];
@@ -276,9 +282,10 @@
 		} else if ([outputFormat isEqualToString:@"insertAsSnippet"]) {
 			[self deleteRange:inputRange];
 			[self setCaret:inputRange.location];
-			[[self delegate] setActiveSnippet:[self insertSnippet:outputText atLocation:[self caret]]];
-		}
-		else if ([outputFormat isEqualToString:@"discard"])
+			[[self delegate] setActiveSnippet:[self insertSnippet:outputText atLocation:inputRange.location]];
+			[self setInsertMode:nil];
+			[self setCaret:final_location];
+		} else if ([outputFormat isEqualToString:@"discard"])
 			;
 		else
 			INFO(@"unknown output format: %@", outputFormat);

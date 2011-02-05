dbe67026eed44bac3b46559e3f9ea575d18f4761
Martin Hedenfalk <martin@bzero.se>
2011-02-05 19:41:53 +0100

    only source controls need background drawn

--- ViBgView.m
+++ ViBgView.m
@@ -6,12 +6,10 @@
 
 - (void)drawRect:(NSRect)rect
 {
-	rect = [self bounds];
-	if (sourceHighlight)
+	if (sourceHighlight) {
 		[[NSColor colorWithDeviceRed:(float)0xDD/0xFF green:(float)0xE4/0xFF blue:(float)0xEB/0xFF alpha:1.0] set];
-	else
-		[[NSColor colorWithDeviceRed:(float)0xED/0xFF green:(float)0xED/0xFF blue:(float)0xED/0xFF alpha:1.0] set];
-	[NSBezierPath fillRect: rect];
+		NSRectFill([self bounds]);
+	}
 }
 
 @end

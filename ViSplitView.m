aa3d7327cc97aad5e29d28ab3404de8a02627727
Martin Hedenfalk <martin@bzero.se>
2011-02-05 19:45:42 +0100

    Make the tab bar enclosed by the main editor view, not above it. Move the explorer search field to the toolbar.

--- ViSplitView.m
+++ ViSplitView.m
@@ -7,5 +7,27 @@
 	return [NSColor grayColor];
 }
 
+- (void)drawDividerInRect:(NSRect)aRect
+{
+	NSRect bg = aRect;
+	bg.size.height = 22;
+	NSDrawWindowBackground(bg);
+	[[NSColor colorWithCalibratedWhite:0.3 alpha:0.2] set];
+	NSRectFillUsingOperation(bg, NSCompositeSourceAtop);
+
+	NSBezierPath *bezier = [NSBezierPath bezierPath];
+	[bezier moveToPoint:NSMakePoint(bg.origin.x, 0.5)];
+	[bezier lineToPoint:NSMakePoint(bg.origin.x + bg.size.width, 0.5)];
+	[bezier moveToPoint:NSMakePoint(bg.origin.x, bg.size.height - 0.5)];
+	[bezier lineToPoint:NSMakePoint(bg.origin.x + bg.size.width, bg.size.height - 0.5)];
+	[[NSColor darkGrayColor] set];
+	[bezier stroke];
+
+	aRect.origin.y += 22;
+	aRect.size.height -= 22;
+
+	[super drawDividerInRect:aRect];
+}
+
 @end
 

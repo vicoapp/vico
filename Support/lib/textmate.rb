ea5d87f0ad6b9a63653c6757c06f8f3c0169e542
Martin Hedenfalk <martin@bzero.se>
2011-03-02 15:45:03 +0100

    more textmake support folder cleanups remove unused and unsupported scripts

--- Support/lib/textmate.rb
+++ Support/lib/textmate.rb
@@ -11,7 +11,7 @@ module TextMate
 
   class << self
     def app_path
-      ENV['TM_APP_PATH'] || %x{ps -xwwp "$TM_PID" -o "command="}.sub(%r{(.app)/Contents/MacOS/.*\n}, '\1')
+      ENV['TM_APP_PATH']
     end
 
     def app_name
@@ -22,16 +22,16 @@ module TextMate
       default_line = options.has_key?(:file) ? 1 : ENV['TM_LINE_NUMBER']
       options = {:file => ENV['TM_FILEPATH'], :line => default_line, :column => 1}.merge(options)
       if options[:file]
-        `open "txmt://open?url=file://#{e_url options[:file]}&line=#{options[:line]}&column=#{options[:column]}"`
+#        `open "txmt://open?url=file://#{e_url options[:file]}&line=#{options[:line]}&column=#{options[:column]}"`
       else
-        `open "txmt://open?line=#{options[:line]}&column=#{options[:column]}"`
+#        `open "txmt://open?line=#{options[:line]}&column=#{options[:column]}"`
       end
     end
 
     def require_cmd(command, message = nil)
       if `which "#{command}"`.empty?
         require ENV['TM_SUPPORT_PATH'] + '/lib/tm/htmloutput'
-        
+
         TextMate::HTMLOutput.show(
           :title      => "Command Not Found",
           :sub_title  => "Command Not Found - #{command}"
@@ -55,7 +55,7 @@ module TextMate
             </ul>
           HTML
         end
-        
+
         TextMate.exit_show_html
       end
     end
@@ -83,149 +83,17 @@ module TextMate
         TextMate.exit_show_html
       end
     end
-    
+
     def min_support(version)
-      actual_version = ::IO.read(ENV['TM_SUPPORT_PATH'] + '/version').to_i
-      if actual_version < version then
-		require "#{ENV['TM_SUPPORT_PATH']}/lib/ui"		
-        TextMate::UI.request_confirmation(:title => "Support Folder is Outdated", :prompt => "Your version of the shared support folder is too old for this action to run.\n\nYou need version #{version} but only have #{actual_version}.", :button1 => "More Info") do
-          help_url = "file://#{e_url(self.app_path + '/Contents/Resources/English.lproj/TextMate Help Book/bundles.html')}#support_folder"
-          %x{ open #{e_sh help_url} }
-          # # unfortuantely the help viewer ignores the fragment specifier of the URL
-          # %x{ osascript <<'APPLESCRIPT'
-          # 	tell app "Help Viewer"
-          # 	  handle url "#{e_as help_url}"
-          # 	  activate
-          # 	end tell	
-          # }
-        end
-        raise SystemExit
-      end
+      # TBD
     end
 
     def rescan_project
-      `osascript &>/dev/null \
-    	   -e 'tell app "SystemUIServer" to activate'; \
-    	 osascript &>/dev/null \
-    	   -e 'tell app "TextMate" to activate' &`
+      # TBD
     end
   end
 
-  class ProjectFileFilter
-    def initialize
-      @file_pattern = load_pattern('OakFolderReferenceFilePattern', '!(/\.(?!htaccess)[^/]*|\.(tmproj|o|pyc)|/Icon\r)$')
-      @folder_pattern = load_pattern('OakFolderReferenceFolderPattern', '!.*/(\.[^/]*|CVS|_darcs|\{arch\}|blib|.*~\.nib|.*\.(framework|app|pbproj|pbxproj|xcode(proj)?|bundle))$')
+  # project file hacks removed
 
-      @text_types = prefs_for_key('OakProjectTextFiles')     || [ ]
-      @text_types.collect! { |ext| '.' + ext }
-      @binary_types = prefs_for_key('OakProjectBinaryFiles') || [ "nib" ]
-      @binary_types.collect! { |ext| '.' + ext }
-    end
-
-    def prefs_for_key (key)
-      prefs_file = "#{ENV['HOME']}/Library/Preferences/com.macromates.textmate.plist"
-      File.open(prefs_file) do |f|
-        return OSX::PropertyList::load(f)[key]
-      end
-    end
-
-    def load_pattern (key, default_pattern)
-      str = prefs_for_key(key)
-      str = default_pattern if str.to_s.empty?
-      if str[0] == ?! then
-        { :regexp => Regexp.new(str[1..-1]), :negate => true }
-      else
-        { :regexp => Regexp.new(str), :negate => false }
-      end
-    end
-
-    def binary? (file)
-      ext = File.extname(file)
-      if @text_types.member?(ext) then
-        false
-      elsif @binary_types.member?(ext) then
-        true
-      else
-        # ask the file shell command about the type
-        case `file #{e_sh file}`
-        when /\bempty\b/ then
-          # treat empty files as binary, but do not record the extension
-          true
-        when /\btext\b/ then
-          @text_types << ext unless ext.empty?
-          false
-        else
-          @binary_types << ext unless ext.empty?
-          true
-        end
-      end
-    end
-
-    def skip? (file)
-      a_directory = File.directory?(file)
-      ptrn = a_directory ? @folder_pattern : @file_pattern
-      skip_it = ptrn[:regexp].match(file) ? ptrn[:negate] : !ptrn[:negate]
-      return (skip_it or a_directory) ? skip_it || File.symlink?(file) : binary?(file)
-    end
-  end
-
-  def TextMate.scan_dir (dir, block, filter)
-    return unless File.executable?(dir)
-    Dir.entries(dir).each do |filename|
-      fullpath = File.join(dir, filename)
-      if(filter.skip?(fullpath)) then
-        # skip hidden files and folders
-      elsif(File.directory?(fullpath)) then
-        scan_dir(fullpath, block, filter)
-      else
-        block.call(fullpath)
-      end
-  	end
-  end
-
-  def TextMate.each_text_file (&block)
-    project_dir = ENV['TM_PROJECT_DIRECTORY']
-    current_file = ENV['TM_FILEPATH']
-
-    if selected_files then
-      selected_files.each do |path|
-        if File.directory? path
-          scan_dir(path, block, ProjectFileFilter.new)
-        else
-          block.call(path)
-        end
-      end
-    elsif project_dir then
-      TextMate.scan_dir(project_dir, block, ProjectFileFilter.new)
-    elsif current_file then
-      block.call(current_file)
-    end
-  end
-  
-  def TextMate.each_text_file_in_project (&block)
-    project_dir = ENV['TM_PROJECT_DIRECTORY']
-    current_file = ENV['TM_FILEPATH']
-
-    if project_dir then
-      TextMate.scan_dir(project_dir, block, ProjectFileFilter.new)
-    elsif current_file then
-      block.call(current_file)
-    end
-  end
-  
-  # returns a array if all currently selected files or nil
-  def TextMate.selected_files( tm_selected_files = ENV['TM_SELECTED_FILES'] )
-    return nil  if tm_selected_files.nil? or tm_selected_files.empty?
-    require 'shellwords'
-    Shellwords.shellwords( tm_selected_files )
-  end
-  
 end
 
-# if $0 == __FILE__ then
-#   `touch /tmp/a.txt /tmp/b.txt`
-#   ENV['TM_PROJECT_DIRECTORY'] = "/tmp"
-#   ENV['TM_SELECTED_FILES'] = "/tmp/a.txt"
-#   TextMate.each_text_file { |f| puts f }
-#   TextMate.each_text_file_in_project { |f| puts f }
-# end

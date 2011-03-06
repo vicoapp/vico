e78c00f52a7a60daa5ea9998d7e7291ddc90a2d3
Martin Hedenfalk <martin@bzero.se>
2011-03-06 18:46:27 +0100

    fix compiler warning

--- oniguruma/regint.h
+++ oniguruma/regint.h
@@ -329,8 +329,7 @@ typedef Bits*          BitSetRef;
 #define SIZE_BITSET        sizeof(BitSet)
 
 #define BITSET_CLEAR(bs) do {\
-  int i;\
-  for (i = 0; i < (int )BITSET_SIZE; i++) { (bs)[i] = 0; }	\
+  for (int _i = 0; _i < (int )BITSET_SIZE; _i++) { (bs)[_i] = 0; }	\
 } while (0)
 
 #define BS_ROOM(bs,pos)            (bs)[pos / BITS_IN_ROOM]

0a030b927c01caae29bfc1367690c2c32165cda0
Martin Hedenfalk <martin@bzero.se>
2011-04-18 12:12:13 +0200

    Locally modified bundles moved to GitHub.

--- Bundles/latex.tmbundle/Support/bin/LatexLabelCompletions.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require ENV["TM_SUPPORT_PATH"] + "/lib/LaTeXUtils.rb"
-phrase = STDIN.read.chomp
-include LaTeX
-items = LaTeX.get_labels
-items = items.grep(phrase) if phrase != ""
-exit if items.empty?
-puts items.join("\n")
--- Bundles/perl.tmbundle/Tests/heredoc_test.pm
+++
@@ -1,5 +0,0 @@
-# Here-doc which currently breaks (Revision: 1416)
-# submitted by Michael Irwin
-$sql .= <<SQL if ( $type eq 'cnd' or $type eq 'mul' );
-LEFT JOIN features AS d6 ON a.Style = d6.UID
-SQL

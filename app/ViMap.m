b2738ccf4f1843a93203ed17d5f35c55955fcf78
Martin Hedenfalk <martin@bzero.se>
2011-05-02 11:38:53 +0200

    don't use default action for keyboard equivalents

--- app/ViMap.m
+++ app/ViMap.m
@@ -479,7 +479,7 @@ static NSMutableDictionary *maps = nil;
 		*outError = error;
 
 	if (m == nil) {
-		if (error.code == ViErrorMapNotFound && defaultAction) {
+		if (error.code == ViErrorMapNotFound && defaultAction && [[keySequence objectAtIndex:0] integerValue] < 0xFFFF) {
 			/* Nothing matched. Return the default action, if there is one. */
 			if (outError)
 				*outError = nil;

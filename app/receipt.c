65b29246f6cadd211a317c20e040ec71380710c6
Martin Hedenfalk <martin@bzero.se>
2011-05-06 13:50:14 +0200

    use openssl's SHA1 implementeation (simplify us crypto export madness)

--- app/receipt.c
+++ app/receipt.c
@@ -20,7 +20,6 @@
 
 #include "receipt.h"
 #include "ber.h"
-#include "sha1.h"
 
 #ifdef DEBUG
 # define DPRINTF(...)	do { fprintf(stderr, "%s:%d: ", __func__, __LINE__); \
@@ -100,21 +99,21 @@ void
 receipt_validate(const char *receipt_path)
 {
 #include "AppleIncRootCertificate.h"
-	u_int8_t		 ca_digest[SHA1_DIGEST_LENGTH];
-	SHA1_CTX		 ca_ctx;
-	u_int8_t		 bundle_fingerprint[SHA1_DIGEST_LENGTH] = {
+	u_int8_t		 ca_digest[SHA_DIGEST_LENGTH];
+	SHA_CTX			 ca_ctx;
+	u_int8_t		 bundle_fingerprint[SHA_DIGEST_LENGTH] = {
 #include "bundle_fingerprint.h"
 	};
-	u_int8_t		 ca_fingerprint[SHA1_DIGEST_LENGTH] = {
+	u_int8_t		 ca_fingerprint[SHA_DIGEST_LENGTH] = {
 		0x61, 0x1e, 0x5b, 0x66, 0x2c, 0x59, 0x3a, 0x08,
 		0xff, 0x58, 0xd1, 0x4a, 0xe2, 0x24, 0x52, 0xd1,
 		0x98, 0xdf, 0x6c, 0x60
 	};
-	SHA1_CTX		 bundle_ctx;
-	u_int8_t		 digest[SHA1_DIGEST_LENGTH];
+	SHA_CTX			 bundle_ctx;
+	u_int8_t		 digest[SHA_DIGEST_LENGTH];
 	u_int8_t		 mac[6];
 	struct ber		 ber;
-	SHA1_CTX		 guid_hash;
+	SHA_CTX			 guid_hash;
 	struct ber_element	*root, *attr;
 	FILE			*fp;
 	void			*value, *payload;
@@ -130,9 +129,9 @@ receipt_validate(const char *receipt_path)
 	ERR_load_crypto_strings();
 
 	for (i = 0; i < root_ca_len; i++) {
-		SHA1Init(&bundle_ctx);
+		SHA1_Init(&bundle_ctx);
 		root_ca_xor[i] ^= root_ca_key[i];
-		SHA1Init(&guid_hash);
+		SHA1_Init(&guid_hash);
 	}
 
 	if (get_node_addr(mac) != 0) {
@@ -143,7 +142,7 @@ receipt_validate(const char *receipt_path)
 	ERR_load_PKCS7_strings();
 
 	HEXDUMP(mac, 6, "adding %i bytes of mac address to guid hash", 6);
-	SHA1Update(&guid_hash, mac, 6);
+	SHA1_Update(&guid_hash, mac, 6);
 
 	DPRINTF("validating receipt in %s", receipt_path);
 
@@ -183,10 +182,10 @@ receipt_validate(const char *receipt_path)
 		_exit(173);
 	}
 
-	SHA1Init(&ca_ctx);
-	SHA1Update(&ca_ctx, root_ca_xor, root_ca_len);
-	SHA1Final(ca_digest, &ca_ctx);
-	if (bcmp(ca_digest, ca_fingerprint, SHA1_DIGEST_LENGTH) != 0)
+	SHA1_Init(&ca_ctx);
+	SHA1_Update(&ca_ctx, root_ca_xor, root_ca_len);
+	SHA1_Final(ca_digest, &ca_ctx);
+	if (bcmp(ca_digest, ca_fingerprint, SHA_DIGEST_LENGTH) != 0)
 		_exit(173);
  
 	/* Initialize b_x509 as an input BIO with a value of the
@@ -266,7 +265,7 @@ receipt_validate(const char *receipt_path)
 			DPRINTF("bundle identifier is %s", (char *)bundle_id);
 			HEXDUMP(bundle_id, root->be_len,
 			    "adding %zu bytes of bundle id to bundle hash", root->be_len);
-			SHA1Update(&bundle_ctx, bundle_id, root->be_len);
+			SHA1_Update(&bundle_ctx, bundle_id, root->be_len);
 			bundle_id = value;
 			bundle_id_len = len;
 		} else if (type == 3) {
@@ -285,7 +284,7 @@ receipt_validate(const char *receipt_path)
 		} else if (type == 5) {
 			HEXDUMP(value, len, "SHA-1 digest");
 			hash_buf = value;
-			if (len != SHA1_DIGEST_LENGTH) {
+			if (len != SHA_DIGEST_LENGTH) {
 				DPRINTF("invalid length of sha-1 digest");
 				_exit(173);
 			}
@@ -294,26 +293,26 @@ receipt_validate(const char *receipt_path)
 
 
 	HEXDUMP(opaque_buf, opaque_len, "adding %zu bytes of opaque data to guid hash", opaque_len);
-	SHA1Update(&guid_hash, opaque_buf, opaque_len);
+	SHA1_Update(&guid_hash, opaque_buf, opaque_len);
 	HEXDUMP(bundle_id, bundle_id_len, "adding %zu bytes of bundle id to guid hash", bundle_id_len);
-	SHA1Update(&guid_hash, bundle_id, bundle_id_len);
+	SHA1_Update(&guid_hash, bundle_id, bundle_id_len);
 
 	HEXDUMP(bundle_ver, bundle_ver_len,
 	    "adding %zu bytes of bundle version to bundle hash", bundle_ver_len);
-	SHA1Update(&bundle_ctx, bundle_ver, bundle_ver_len);
+	SHA1_Update(&bundle_ctx, bundle_ver, bundle_ver_len);
 
-	SHA1Final(ca_digest, &bundle_ctx); /* XXX: re-using ca_digest buffer */
-	SHA1Final(digest, &guid_hash);
+	SHA1_Final(ca_digest, &bundle_ctx); /* XXX: re-using ca_digest buffer */
+	SHA1_Final(digest, &guid_hash);
 
-	HEXDUMP(ca_digest, SHA1_DIGEST_LENGTH, "computed bundle hash");
-	HEXDUMP(bundle_fingerprint, SHA1_DIGEST_LENGTH, "stored bundle hash");
-	if (bcmp(ca_digest, bundle_fingerprint, SHA1_DIGEST_LENGTH) != 0) {
+	HEXDUMP(ca_digest, SHA_DIGEST_LENGTH, "computed bundle hash");
+	HEXDUMP(bundle_fingerprint, SHA_DIGEST_LENGTH, "stored bundle hash");
+	if (bcmp(ca_digest, bundle_fingerprint, SHA_DIGEST_LENGTH) != 0) {
 		DPRINTF("bundle id/version fingerprint don't match");
 		_exit(173);
 	}
 
-	HEXDUMP(digest, SHA1_DIGEST_LENGTH, "computed guid hash");
-	if (bcmp(digest, hash_buf, SHA1_DIGEST_LENGTH) != 0) {
+	HEXDUMP(digest, SHA_DIGEST_LENGTH, "computed guid hash");
+	if (bcmp(digest, hash_buf, SHA_DIGEST_LENGTH) != 0) {
 		DPRINTF("guid hash doesn't match");
 		_Exit(173);
 	}

bb5e66895cd6f8fa805a47dfc77a6c49dccfe197
Martin Hedenfalk <martin@bzero.se>
2011-09-21 21:12:22 +0200

    MRC; note that sheets are Asynchronous; refactor writing temporary files for shebang shell scripts into a function should be callable from snippets and bundle commands

--- app/ViTaskRunner.h
+++ app/ViTaskRunner.h
@@ -2,28 +2,33 @@
 
 @interface ViTaskRunner : NSObject <NSStreamDelegate>
 {
-	NSTask			*task;
-	NSWindow		*window;
-	ViBufferedStream	*stream;
-	NSMutableData		*stdout, *stderr;
-	int			 status;
-	BOOL			 done, failed, cancelled;
-	id			 target;
-	SEL			 selector;
-	id			 contextInfo;
+	NSTask			*_task;
+	NSWindow		*_window;
+	ViBufferedStream	*_stream;
+	NSMutableData		*_stdout;
+	NSMutableData		*_stderr;
+	int			 _status;
+	BOOL			 _done;
+	BOOL			 _failed;
+	BOOL			 _cancelled;
+	id			 _target;
+	SEL			 _selector;
+	id			 _contextInfo;
 
 	/* Blocking for completion. */
-	IBOutlet NSWindow	*waitWindow;
+	IBOutlet NSWindow	*waitWindow; // Top-level nib object
 	IBOutlet NSButton	*cancelButton;
 	IBOutlet NSProgressIndicator *progressIndicator;
 	IBOutlet NSTextField	*waitLabel;
 }
 
-@property (nonatomic, readonly) NSTask *task;
-@property (nonatomic, readonly) NSWindow *window;
-@property (nonatomic, readonly) ViBufferedStream *stream;
-@property (nonatomic, readonly) NSMutableData *stdout;
-@property (nonatomic, readonly) NSMutableData *stderr;
+@property (nonatomic, readwrite, retain) NSTask *task;
+@property (nonatomic, readwrite, retain) NSWindow *window;
+@property (nonatomic, readwrite, retain) ViBufferedStream *stream;
+@property (nonatomic, readwrite, retain) NSMutableData *standardOutput;
+@property (nonatomic, readwrite, retain) NSMutableData *standardError;
+@property (nonatomic, readwrite, retain) id contextInfo;
+@property (nonatomic, readwrite, retain) id target;
 @property (nonatomic, readonly) int status;
 @property (nonatomic, readonly) BOOL cancelled;
 
@@ -31,11 +36,22 @@
 
 - (void)launchTask:(NSTask *)aTask
  withStandardInput:(NSData *)stdin
-synchronouslyInWindow:(NSWindow *)aWindow
+asynchronouslyInWindow:(NSWindow *)aWindow
 	     title:(NSString *)displayTitle
 	    target:(id)aTarget
 	  selector:(SEL)aSelector
        contextInfo:(id)contextObject;
+
+- (BOOL)launchShellCommand:(NSString *)shellCommand
+	 withStandardInput:(NSData *)stdin
+	       environment:(NSDictionary *)environment
+	  currentDirectory:(NSString *)currentDirectory
+    asynchronouslyInWindow:(NSWindow *)aWindow
+		     title:(NSString *)displayTitle
+		    target:(id)aTarget
+		  selector:(SEL)aSelector
+	       contextInfo:(id)contextObject
+		     error:(NSError **)outError;
 
 - (IBAction)cancelTask:(id)sender;
 
--- app/ViTaskRunner.m
+++ app/ViTaskRunner.m
@@ -5,62 +5,94 @@
 #import "ViCommon.h"
 #include "logging.h"
 
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 @implementation ViTaskRunner
 
-@synthesize task, window, stream, stdout, stderr, status, cancelled;
+@synthesize task = _task;
+@synthesize window = _window;
+@synthesize stream = _stream;
+@synthesize standardOutput = _stdout;
+@synthesize standardError = _stderr;
+@synthesize contextInfo = _contextInfo;
+@synthesize target = _target;
+@synthesize status = _status;
+@synthesize cancelled = _cancelled;
 
 - (ViTaskRunner *)init
 {
 	if ((self = [super init]) != nil) {
-		[NSBundle loadNibNamed:@"WaitProgress" owner:self];
+		if (![NSBundle loadNibNamed:@"WaitProgress" owner:self]) {
+			[self release];
+			return nil;
+		}
 	}
 	return self;
+}
+
+- (void)dealloc
+{
+	INFO(@"%p", self);
+	if ([_task isRunning])
+		kill([_task processIdentifier], SIGKILL);
+	[_task release];
+	[_window release];
+	[_stream release];
+	[_stdout release];
+	[_stderr release];
+	[_contextInfo release];
+	[_target release];
+	[waitWindow release]; // Top-level nib object
+	[super dealloc];
 }
 
 - (NSString *)stdoutString
 {
 	/* Try to auto-detect the encoding. */
-	NSStringEncoding encoding = [[ViCharsetDetector defaultDetector] encodingForData:stdout];
+	NSStringEncoding encoding = [[ViCharsetDetector defaultDetector] encodingForData:_stdout];
 	if (encoding == 0)
 		/* Try UTF-8 if auto-detecting fails. */
 		encoding = NSUTF8StringEncoding;
-	NSString *outputText = [[NSString alloc] initWithData:stdout encoding:encoding];
+	NSString *outputText = [[NSString alloc] initWithData:_stdout encoding:encoding];
 	if (outputText == nil) {
 		/* If all else fails, use iso-8859-1. */
 		encoding = NSISOLatin1StringEncoding;
-		outputText = [[NSString alloc] initWithData:stdout encoding:encoding];
+		outputText = [[NSString alloc] initWithData:_stdout encoding:encoding];
 	}
 
-	return outputText;
+	return [outputText autorelease];
 }
 
 - (void)finish
 {
-	if (![task isRunning])
-		DEBUG(@"task %@ is no longer running", task);
+	if (![_task isRunning])
+		DEBUG(@"task %@ is no longer running", _task);
 	else {
-		DEBUG(@"wait until exit of task %@", task);
-		[task waitUntilExit];
+		DEBUG(@"wait until exit of task %@", _task);
+		[_task waitUntilExit];
 	}
-	status = [task terminationStatus];
-	DEBUG(@"status = %d", status);
+	_status = [_task terminationStatus];
+	DEBUG(@"status = %d", _status);
 
-	[stream close];
+	[_stream close];
 
-	if (target && selector) {
-		NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[target methodSignatureForSelector:selector]];
-		[invocation setSelector:selector];
+	if (_target && _selector) {
+		NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[_target methodSignatureForSelector:_selector]];
+		[invocation setSelector:_selector];
 		[invocation setArgument:&self atIndex:2];
-		[invocation setArgument:&status atIndex:3];
-		[invocation setArgument:&contextInfo atIndex:4];
-		[invocation invokeWithTarget:target];
+		[invocation setArgument:&_status atIndex:3];
+		[invocation setArgument:&_contextInfo atIndex:4];
+		[invocation invokeWithTarget:_target];
 	}
 
-	stream = nil;
-	stdout = nil;
-	stderr = nil;
-	target = nil;
-	contextInfo = nil;
+	[self setContextInfo:nil];
+	[self setStandardOutput:nil];
+	[self setStandardError:nil];
+	[self setStream:nil];
+	[self setTarget:nil];
+	[self setWindow:nil];
 }
 
 - (void)waitSheetDidEnd:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo
@@ -68,8 +100,8 @@
 	[sheet orderOut:self];
 
 	if (returnCode == -1) {
-		DEBUG(@"terminating filter task %@", task);
-		[task terminate];
+		DEBUG(@"terminating filter task %@", _task);
+		[_task terminate];
 	}
 
 	[progressIndicator stopAnimation:self];
@@ -78,8 +110,11 @@
 
 - (IBAction)cancelTask:(id)sender
 {
-	cancelled = YES;
-	[NSApp endSheet:waitWindow returnCode:-1];
+	_cancelled = YES;
+	if (_window)
+		[NSApp endSheet:waitWindow returnCode:-1];
+	else
+		[NSApp abortModal];
 }
 
 - (void)stream:(NSStream *)aStream handleEvent:(NSStreamEvent)event
@@ -95,61 +130,64 @@
 	default:
 		break;
 	case NSStreamEventHasBytesAvailable:
-		[stream getBuffer:&ptr length:&len];
+		[_stream getBuffer:&ptr length:&len];
 		DEBUG(@"got %lu bytes", len);
 		if (len > 0)
-			[stdout appendBytes:ptr length:len];
+			[_stdout appendBytes:ptr length:len];
 		break;
 	case NSStreamEventHasSpaceAvailable:
 		/* All output data flushed. */
-		[stream shutdownWrite];
-		[[[task standardInput] fileHandleForWriting] closeFile];
+		[_stream shutdownWrite];
+		[[[_task standardInput] fileHandleForWriting] closeFile];
 		break;
 	case NSStreamEventErrorOccurred:
-		INFO(@"error on stream %@: %@", stream, [stream streamError]);
-		if ([window attachedSheet] != nil)
+		INFO(@"error on stream %@: %@", _stream, [_stream streamError]);
+		if (_window == nil)
+			[NSApp abortModal];
+		else if ([_window attachedSheet] != nil)
 			[NSApp endSheet:waitWindow returnCode:-1];
-		failed = 1;
+		_failed = 1;
 		break;
 	case NSStreamEventEndEncountered:
-		DEBUG(@"EOF on stream %@", stream);
-		if ([window attachedSheet] != nil)
+		DEBUG(@"EOF on stream %@", _stream);
+		if (_window == nil)
+			[NSApp abortModal];
+		else if ([_window attachedSheet] != nil)
 			[NSApp endSheet:waitWindow returnCode:0];
-		done = YES;
+		_done = YES;
 		break;
 	case ViStreamEventWriteEndEncountered:
-		DEBUG(@"EOF on write stream %@; we keep reading", stream);
+		DEBUG(@"EOF on write stream %@; we keep reading", _stream);
 		break;
 	}
 }
 
 - (void)launchTask:(NSTask *)aTask
  withStandardInput:(NSData *)stdin
-synchronouslyInWindow:(NSWindow *)aWindow
+asynchronouslyInWindow:(NSWindow *)aWindow
 	     title:(NSString *)displayTitle
 	    target:(id)aTarget
 	  selector:(SEL)aSelector
        contextInfo:(id)contextObject
 {
-	NSParameterAssert(aWindow);
 	NSParameterAssert(displayTitle);
-	NSAssert(![task isRunning], @"Task is already running");
+	NSAssert(![_task isRunning], @"Task is already running");
 
-	task = aTask;
-	window = aWindow;
-	stdout = [NSMutableData data];
-	stderr = [NSMutableData data];
+	[self setTask:aTask];
+	[self setWindow:aWindow];
+	[self setTarget:aTarget];
+	[self setStandardOutput:[NSMutableData data]];
+	[self setStandardError:[NSMutableData data]];
+	[self setContextInfo:contextObject];
 
-	stream = [task scheduledStreamWithStandardInput:stdin captureStandardError:YES];
-	[stream setDelegate:self];
+	_status = -1;
+	_done = NO;
+	_failed = NO;
+	_cancelled = NO;
+	_selector = aSelector;
 
-	status = -1;
-	done = NO;
-	failed = NO;
-	cancelled = NO;
-	target = aTarget;
-	selector = aSelector;
-	contextInfo = contextObject;
+	[self setStream:[_task scheduledStreamWithStandardInput:stdin captureStandardError:YES]];
+	[_stream setDelegate:self];
 
 	NSDate *limitDate = [NSDate dateWithTimeIntervalSinceNow:2.0];
 
@@ -161,29 +199,115 @@ synchronouslyInWindow:(NSWindow *)aWindow
 			break;
 		}
 
-		if (failed) {
+		if (_failed) {
 			DEBUG(@"%s", "filter I/O failed");
-			[task terminate];
-			done = YES;
+			[_task terminate];
+			_done = YES;
 			break;
 		}
 
-		if (done)
+		if (_done)
 			break;
 	}
 
-	if (done) {
+	if (_done) {
 		[self finish];
 	} else {
-		[NSApp beginSheet:waitWindow
-                   modalForWindow:window
-                    modalDelegate:self
-                   didEndSelector:@selector(waitSheetDidEnd:returnCode:contextInfo:)
-                      contextInfo:NULL];
+		[waitWindow setTitle:@"Waiting on shell command"];
 		[waitLabel setStringValue:displayTitle];
 		[waitLabel setFont:[NSFont userFixedPitchFontOfSize:12.0]];
 		[progressIndicator startAnimation:self];
+		if (_window) {
+			[NSApp beginSheet:waitWindow
+			   modalForWindow:_window
+			    modalDelegate:self
+			   didEndSelector:@selector(waitSheetDidEnd:returnCode:contextInfo:)
+			      contextInfo:NULL];
+		} else {
+			[_stream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSModalPanelRunLoopMode];
+			NSInteger ret = [NSApp runModalForWindow:waitWindow];
+			INFO(@"modal returned %li", ret);
+			[waitWindow orderOut:nil];
+			[self waitSheetDidEnd:nil
+				   returnCode:(_cancelled || _failed) ? -1 : 0
+				  contextInfo:NULL];
+		}
 	}
+}
+
+- (BOOL)launchShellCommand:(NSString *)shellCommand
+	 withStandardInput:(NSData *)stdin
+	       environment:(NSDictionary *)environment
+	  currentDirectory:(NSString *)currentDirectory
+    asynchronouslyInWindow:(NSWindow *)aWindow
+		     title:(NSString *)displayTitle
+		    target:(id)aTarget
+		  selector:(SEL)aSelector
+	       contextInfo:(id)contextObject
+		     error:(NSError **)outError
+{
+	char *templateFilename = NULL;
+	int fd = -1;
+
+	DEBUG(@"shell command = [%@]", shellCommand);
+	if ([shellCommand hasPrefix:@"#!"]) {
+		const char *tmpl = [[NSTemporaryDirectory()
+		    stringByAppendingPathComponent:@"vico_cmd.XXXXXXXXXX"]
+		    fileSystemRepresentation];
+		DEBUG(@"using template %s", tmpl);
+		templateFilename = strdup(tmpl);
+		fd = mkstemp(templateFilename);
+		if (fd == -1) {
+			if (outError)
+				*outError = [ViError errorWithFormat:@"Failed to open temporary file: %s", strerror(errno)];
+			return NO;
+		}
+		const char *data = [shellCommand UTF8String];
+		ssize_t rc = write(fd, data, strlen(data));
+		DEBUG(@"wrote %i byte", rc);
+		if (rc == -1) {
+			if (outError)
+				*outError = [ViError errorWithFormat:@"Failed to save temporary command file: %s", strerror(errno)];
+			unlink(templateFilename);
+			close(fd);
+			free(templateFilename);
+			return NO;
+		}
+		chmod(templateFilename, 0700);
+		NSFileManager *fm = [NSFileManager defaultManager];
+		shellCommand = [fm stringWithFileSystemRepresentation:templateFilename
+							       length:strlen(templateFilename)];
+	}
+
+	NSTask *task = [[[NSTask alloc] init] autorelease];
+	if (templateFilename)
+		[task setLaunchPath:shellCommand];
+	else {
+		[task setLaunchPath:@"/bin/bash"];
+		[task setArguments:[NSArray arrayWithObjects:@"-c", shellCommand, nil]];
+	}
+
+	[task setEnvironment:environment];
+	if (currentDirectory)
+		[task setCurrentDirectoryPath:currentDirectory];
+	else
+		[task setCurrentDirectoryPath:NSTemporaryDirectory()];
+
+	[self launchTask:task
+	 withStandardInput:stdin
+    asynchronouslyInWindow:aWindow
+		     title:displayTitle
+		    target:aTarget
+		  selector:aSelector
+	       contextInfo:contextObject];
+
+	if (fd != -1) {
+		unlink(templateFilename);
+		close(fd);
+		free(templateFilename);
+	}
+
+	return YES;
 }
 
 @end

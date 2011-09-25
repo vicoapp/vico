6e4d2ef6f05835948274528094ced5e7b7d33aec
Martin Hedenfalk <martin@bzero.se>
2011-09-25 19:01:09 +0200

    MRC

--- app/ViDocumentView.h
+++ app/ViDocumentView.h
@@ -5,17 +5,16 @@
 // XXX: actually a view _controller_
 @interface ViDocumentView : NSObject <ViViewController>
 {
-	IBOutlet NSView *view;
-	IBOutlet NSView *innerView;
-	IBOutlet NSScrollView *scrollView;
-	ViDocument *document;
-	ViTabController *tabController;
+	IBOutlet NSView		*_view;
+	IBOutlet NSView		*_innerView;
+	IBOutlet NSScrollView	*_scrollView;
+	ViDocument		*_document;
+	ViTabController		*_tabController;
 }
 
-@property(nonatomic,readonly) id<ViViewDocument> document;
 @property(nonatomic,readonly) NSView *view;
 @property(nonatomic,readonly) NSView *innerView;
-@property(nonatomic,readwrite, assign) ViTabController *tabController;
+@property(nonatomic,readwrite,assign) ViTabController *tabController;
 
 - (ViDocumentView *)initWithDocument:(ViDocument *)aDocument;
 - (ViTextView *)textView;
--- app/ViDocumentView.m
+++ app/ViDocumentView.m
@@ -4,37 +4,55 @@
 
 @implementation ViDocumentView
 
-@synthesize view;
-@synthesize innerView;
-@synthesize document;
-@synthesize tabController;
+@synthesize view = _view;
+@synthesize innerView = _innerView;
+@synthesize document = _document;
+@synthesize tabController = _tabController;
 
 - (ViDocumentView *)initWithDocument:(ViDocument *)aDocument
 {
-	self = [super init];
-	if (self) {
+	if ((self = [super init]) != nil) {
 		if (![NSBundle loadNibNamed:@"ViDocument" owner:self]) {
 			INFO(@"%s", "Failed to load nib \"ViDocument\"");
 			return nil;
 		}
-		document = aDocument;
-		[document addObserver:self
-		           forKeyPath:@"title"
-		              options:NSKeyValueObservingOptionNew
-		              context:nil];
+		DEBUG(@"init document view %p", self);
+		[self setDocument:aDocument];
 	}
 	return self;
+}
+
+- (void)dealloc
+{
+	DEBUG_DEALLOC();
+	if (_document)
+		[self setDocument:nil];
+	[_view release]; // Top-level nib object
+	[super dealloc];
+}
+
+- (void)setDocument:(ViDocument *)document
+{
+	DEBUG(@"set document %@ -> %@", _document, document);
+	[_document removeObserver:self forKeyPath:@"title"];
+	[document retain];
+	[_document release];
+	_document = document;
+	[_document addObserver:self
+		    forKeyPath:@"title"
+		       options:NSKeyValueObservingOptionNew
+		       context:nil];
 }
 
 - (NSString *)description
 {
 	return [NSString stringWithFormat:@"<ViDocumentView %p: %@>",
-	    self, (id)[document fileURL] ?: (id)@"<Untitled>"];
+	    self, _document ? [_document description] : @"<Untitled>"];
 }
 
 - (ViTextView *)textView
 {
-	return (ViTextView *)innerView;
+	return (ViTextView *)_innerView;
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath
@@ -50,19 +68,19 @@
 
 - (NSString *)title
 {
-	return [document title];
+	return [_document title];
 }
 
 - (void)replaceTextView:(ViTextView *)textView
 {
-	[innerView removeFromSuperview];
-	[scrollView setDocumentView:textView];
+	[_innerView removeFromSuperview];
+	_innerView = textView;
+	[_scrollView setDocumentView:_innerView];
 	[textView setAutoresizingMask:NSViewHeightSizable | NSViewWidthSizable];
 	[textView setMinSize:NSMakeSize(83, 0)];
 	[textView setMaxSize:NSMakeSize(FLT_MAX, FLT_MAX)];
 	[textView setVerticallyResizable:YES];
 	[textView setHorizontallyResizable:YES];
-	innerView = textView;
 }
 
 @end

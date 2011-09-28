5c7e1e9f860b84da641e5799d229dc24ac1b5818
Martin Hedenfalk <martin@bzero.se>
2011-09-28 08:57:34 +0200

    utility method for returning first responder, or delegate if first responder is a field editor

--- app/ViWindow.h
+++ app/ViWindow.h
@@ -2,5 +2,6 @@
 {
 }
 - (BOOL)isFullScreen;
+- (id)firstResponderOrDelegate;
 @end
 
--- app/ViWindow.m
+++ app/ViWindow.m
@@ -29,4 +29,12 @@ DEBUG_FINALIZE();
 	return ([self styleMask] & NSFullScreenWindowMask) == NSFullScreenWindowMask;
 }
 
+- (id)firstResponderOrDelegate
+{
+	id resp = [self firstResponder];
+	if ([resp isKindOfClass:[NSTextView class]] && resp == [self fieldEditor:NO forObject:nil] != nil)
+		resp = [(NSTextView *)resp delegate];
+	return resp;
+}
+
 @end

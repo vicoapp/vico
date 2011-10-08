08c466e185c3d90ca9ba7eea2c6d600e9d611b8f
Martin Hedenfalk <martin@bzero.se>
2011-10-08 16:27:21 +0200

    mark nu_log function with visibility("default")

--- app/main.m
+++ app/main.m
@@ -15,7 +15,7 @@ pthread_mutex_t onig_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 struct timeval launch_start;
 
-void
+__attribute__((visibility("default"))) void
 nu_log(NSString *msg)
 {
 	NSLog(@"%@", msg);

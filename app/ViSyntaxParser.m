d27d7b141dfd372a502d60ac8b45e9671bb07243
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2013-10-24 23:38:41 -0400

    Hack fix so complex capture highlighting doesn't kill Vico.
    Right now a capture highlight that contains more than just
    a name for the capture group makes Vico enter an
    unrecoverable state. We guard for the `name` key being
    present in the capture object before trying to use it.
    
    This is a stopgap fix until we implement proper support for
    more complex capture highlight definitions.

--- app/ViSyntaxParser.m
+++ app/ViSyntaxParser.m
@@ -356,7 +356,7 @@
 	for (key in [captures allKeys]) {
 		NSDictionary *capture = [captures objectForKey:key];
 		NSRange r = [aMatch rangeOfSubstringAtIndex:[key intValue]];
-		if (r.length > 0) {
+		if (r.length > 0 && [capture objectForKey:@"name"]) {
 			DEBUG(@"got capture [%@] at %u + %u", [capture objectForKey:@"name"], r.location, r.length);
 			[self setScopes:[NSArray arrayWithObject:[capture objectForKey:@"name"]] inRange:r additive:YES];
 		}

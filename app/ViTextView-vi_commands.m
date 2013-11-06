23955f13a11c3f0bb9ca3e479fc0cc3a72fa14ca
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2013-11-05 20:22:01 -0500

    presentCompletions:... does nothing if the completion window is already up.

--- app/ViTextView-vi_commands.m
+++ app/ViTextView-vi_commands.m
@@ -3136,6 +3136,9 @@ again:
 		   fromRange:(NSRange)range
 		     options:(NSString *)options
 {
+	if (_showingCompletionWindow)
+		return NO;
+
 	BOOL fuzzyTrigger = ([options rangeOfString:@"F"].location != NSNotFound);
 
 	/* Present a list to choose from. */

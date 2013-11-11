26b69fc150e98cfa7708d4e16247cadfaecc8c46
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2013-11-11 00:08:16 -0500

    Add depth property to ViFold.

--- app/ViFold.h
+++ app/ViFold.h
@@ -3,7 +3,8 @@
  * important information is the start and end of the fold, and the parent
  * fold (if any), as well as the child folds (if any). It also stores the
  * fold depth, which is a number indicating how many parents exist higher
- * in the fold hierarchy above this fold.
+ * in the fold hierarchy above this fold. A top-level fold should have a
+ * depth of 0.
  *
  * It also stores state information; specifically, whether the fold is open
  * or closed.
@@ -16,6 +17,7 @@
 @property (nonatomic) NSRange range;
 @property (nonatomic) BOOL isOpen;
 @property (nonatomic) ViFold *parent;
+@property (nonatomic) NSUInteger depth;
 @property (nonatomic,readonly) NSSet *children;
 
 + (ViFold *)foldWithRange:(NSRange)range;

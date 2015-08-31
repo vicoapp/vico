520010d713fe8fe6fea1c4aa4579aa1dd568aa78
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2015-08-30 22:31:15 -0400

    Provide typed access to untyped Dictionary values.
    Right now, some Cocoa dictionaries are of AnyObject types. For example,
    NSNotification’s userInfo is a [String: AnyObject]. We extend
    Dictionary to provide safe typed get operations on these, which, when
    you request a T, will return a T?, nil if the key is either missing or
    has the wrong type.
    
    We also provide a typedGet for Int that will automatically unwrap an
    NSNumber.

--- app/Dictionary-typedAccess.swift
+++ app/Dictionary-typedAccess.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+// Extensions for providing typed access to Dictionary keys without
+// explicit casting in client code.
+//
+// Meant to ease interaction with things like NSNotification userInfo
+// dictionaries.
+extension Dictionary { // FIXME limit to AnyObject dictionary somehow...
+    func typedGet<T>(key: Key) -> T? {
+        return self[key] as? T
+    }
+    func typedGet(key: Key) -> Int? {
+        let number: NSNumber? = typedGet(key)
+        
+        return number?.unsignedIntegerValue
+    }
+}
